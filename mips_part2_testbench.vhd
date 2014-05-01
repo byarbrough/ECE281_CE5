fa2923cfe5d2371e7de698ca83367262053ccfdb
Brian Yarbrough <C16brian.yarbrough@usafa.edu>
2014-05-01 10:59:27 -0600

    need my testbook

--- mips_part2_testbench.vhd
+++ mips_part2_testbench.vhd
@@ -93,10 +93,17 @@ BEGIN
 		wait for clk_period/2;
    end process;
  
+----------------------------------------------------
+--MIPS Code
+--	addi $s0, $0, 44
+--	addi $s1, $0, -37
+--	addi $s2, $s0, $s1
+--	sw $s2, 0x54($0)
+------------------------------------------------------
 
    -- Stimulus process
    stim_proc: process
-   begin		
+   begin
      wait for clk_period;
 	  instr <= 
 	  wait for clk_period;
